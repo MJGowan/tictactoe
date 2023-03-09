71ca696241fdff734505367279369444e3442043
Madeline <madelinejgowan@gmail.com>
2023-03-09 15:36:44 -0800

    Almost Forgot To Add Name, Date, Etc. 3:36PM

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+//Madeline Gowan
+//3/9/2023
+//Tic Tac Toe
+//Remake the Tic Tac Toe tutorial with React
+  // -Must have a component folder ( square component and board component)
+  // -Must have a special feature
+  // -Upload to github
+  // -You can use bootstrap or tailwind
+  
+
 import './App.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import GameComponent from './components/game/game';
